a2405e8db82cb421c49ed9d8343e287ad7f864c2
luftwaffe1985 <47695174+luftwaffe1985@users.noreply.github.com>
2022-09-13 16:49:09 +0300

    Added Task047

---
+++ Task047/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+try
+{
+Console.Write("Задайте количество строк двумерного массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте количество столбцов двумерного массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] twoDimensionArray = new double[m, n];
+
+void FillArray(double[,] matrix)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = Convert.ToDouble(new Random().Next(-10, 10) / 10.0);
+        }
+    }
+}
+
+void PrintArray(double[,] matrix)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            Console.Write($"{matrix[i, j]} ||");
+        }
+        Console.WriteLine();
+    }
+}
+FillArray(twoDimensionArray);
+Console.WriteLine();
+PrintArray(twoDimensionArray);
+}
+catch
+{
+    Console.WriteLine("Нужно задать размеры массива цифрами");
+}
