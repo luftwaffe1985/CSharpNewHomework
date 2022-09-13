703d5a22f1ed7eec71e1f6b41927efa754fc02a5
luftwaffe1985 <47695174+luftwaffe1985@users.noreply.github.com>
2022-09-13 18:14:46 +0300

    Added Task052

---
+++ Task052/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] FillArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+            array[i, j] = new Random().Next(1, 10);
+    }
+    return array;
+}
+try
+{
+Console.Write("Введите количество строк двумерного массива ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество стобцов двумерного массива ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] mas = FillArray(m, n);
+PrintArray(mas);
+
+for (int i = 0; i < n; i++)
+{
+    float avg = 0;
+    for (int j = 0; j < m; j++)
+    {
+        avg += mas[j, i];
+    }
+    Console.WriteLine($"Cреднее арифметическое элементов столбца {i + 1} = {Math.Round(avg / m, 2)}");
+}
+}
+catch
+{
+    Console.WriteLine("Надо вводить значения цифрами");
+}
